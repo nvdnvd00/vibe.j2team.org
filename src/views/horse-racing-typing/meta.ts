c312a955e7bdc7bd1b77a72cb959e916616b3645
JUNO_OKYO <junookyo@gmail.com>
2026-03-10 02:05:16 +0700

    fix(horse-racing-typing): add missing category field

--- src/views/horse-racing-typing/meta.ts
+++ src/views/horse-racing-typing/meta.ts
@@ -1,9 +1,10 @@
-import type { PageMeta } from "@/types/page";
+import type { PageMeta } from '@/types/page'
 
 const meta: PageMeta = {
-  name: "Đua Ngựa Typing",
-  description: "Game typing race - gõ phím nhanh để điều khiển ngựa về đích trước!",
-  author: "Dino",
-};
+  name: 'Đua Ngựa Typing',
+  description: 'Game typing race - gõ phím nhanh để điều khiển ngựa về đích trước!',
+  author: 'Dino',
+  category: 'game',
+}
 
-export default meta;
+export default meta
